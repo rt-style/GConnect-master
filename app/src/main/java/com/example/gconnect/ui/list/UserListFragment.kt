7c06b76eaab54dfb840d499cbb65d0245437d108
rt-style <92050479+rt-style@users.noreply.github.com>
2021-10-16 11:56:00 +0530

    Update UserListFragment.kt

--- app/src/main/java/com/example/gconnect/ui/list/UserListFragment.kt
+++ app/src/main/java/com/example/gconnect/ui/list/UserListFragment.kt
@@ -24,13 +24,15 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import java.util.*
 
+//data class Connection(val id : String? =null, val userName: String? = null, val userEmail: String? = null,
+//                      val headline: String? = null, val fullName: String? = null,
+//                      val age: String?=null, val contact: String?=null, val addresses: String? = null,
+//                      val education: String? = null, val skills: String? = null)
 
 data class  Connection ( val id: String? = null, val fullName: String? =null, var headline: String?=null)
 
 data class Notification( val eventId: String? = null, val fromUid : String? = null, val toUid: String? = null,
-                        val fromEmail: String? = null, val message: String? = null, val sender: String? = null,
-                         val receiver : String? = null )
-
+                        val fromEmail: String? = null, val message: String? = null, val sender: String? = null, val receiver : String? = null )
 
 class UserListFragment : Fragment() {
 
@@ -41,7 +43,6 @@ class UserListFragment : Fragment() {
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-
     private val binding get() = _binding!!
     lateinit var rView : RecyclerView
     var connectList = mutableListOf<Connection>()
@@ -72,8 +73,10 @@ class UserListFragment : Fragment() {
         rView.setHasFixedSize(true)
         connectList = arrayListOf()
 
+
         getUserListData()
     }
+
     private fun notification(selectedUser : Connection) {
 
         val stdRef = database.getReference("Users").child("Individual").child("${auth.currentUser?.uid}")
@@ -81,17 +84,24 @@ class UserListFragment : Fragment() {
         stdRef.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 val fullName = snapshot.child("fullName").getValue()
+                val Receiver = selectedUser.fullName
+                val count = snapshot.child("Notification").childrenCount
+//                Log.d("Count","count: $count")
                 val key = stdRef?.push()?.key
                 val notificationNode = database.getReference("Users").child("Notification")
                 val individualUser = Notification(key ,auth.currentUser?.uid, selectedUser.id.toString()
-                    ,auth.currentUser?.email,"$fullName is Trying to connect with you","$fullName", "${selectedUser.fullName}")
+                    ,auth.currentUser?.email,"$fullName is Trying to connect with you","$fullName", "$Receiver")
                 notificationNode.child(individualUser.eventId.toString()).setValue(individualUser)
+//                Log.d("CheckOnDataChange", "$individualUser")
             }
 
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
         })
+
+
+
     }
 
     private fun getUserListData() {
@@ -105,49 +115,70 @@ class UserListFragment : Fragment() {
                    snapshot.children.forEach{
                        val std = it.getValue(Connection::class.java)
                        if(std?.id != auth.currentUser?.uid) {
-                           tempArrayList.add(std!!)
+                           connectList.add(std!!)
+                           tempArrayList.addAll(connectList)
                        }
+
+                       //Log.d("CheckOnDataChange","$connectList")
                    }
                     rView.adapter = UserAdapter(tempArrayList, ::notification)
+//                   Log.d("CheckOnDataChange", "1 : $connectList")
                 }
             }
+
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
-        })
-    }
 
+        })
+//        Log.d("CheckOnDataChange", "2 : $connectList")
+
+    }
     @SuppressLint("ResourceType")
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
         inflater.inflate(R.menu.connection_search,menu)
         val item = menu.findItem(R.id.searchOption)
         val seachView = item.actionView as SearchView
-        seachView.setOnQueryTextListener(object  : SearchView.OnQueryTextListener {
+        seachView.setOnQueryTextListener(object  : SearchView.OnQueryTextListener{
+
             override fun onQueryTextSubmit(query: String?): Boolean {
                 TODO("Not yet implemented")
             }
+
             @SuppressLint("NotifyDataSetChanged")
             override fun onQueryTextChange(newText: String?): Boolean {
+
+
                 tempArrayList.clear()
                 val searchText  = newText!!.toLowerCase(Locale.getDefault())
                 if(searchText.isNotEmpty()){
+
                     connectList.forEach{
+
                         if (it.fullName?.toLowerCase(Locale.getDefault())?.contains(searchText)!!){
                             tempArrayList.add(it)
                         }
                     }
+
                     rView.adapter?.notifyDataSetChanged()
                 }
+
                 else{
+
                     tempArrayList.clear()
                     tempArrayList.addAll(connectList)
                     rView.adapter?.notifyDataSetChanged()
                 }
                 return false
             }
+
         })
+
+
         super.onCreateOptionsMenu(menu, inflater)
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
